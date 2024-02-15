7258c67c8ee6fb31374e253d4ba21413120a0a86
Zakaria-morjani <zakaria.morjani01@gmail.com>
2024-02-15 14:01:01 -0500

    fin

--- PO1_ConstructeurMemoire/Equipe.cs
+++ PO1_ConstructeurMemoire/Equipe.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace PO1_ConstructeurMemoire
 {
-    public class Equipe
+    public class Equipe //click droit puis action rapide et ... puis generer le constructeur
     {
         private string nom;
         private string ville;
 
-       
+        public Equipe(string nom, string ville)
+        {
+            Nom = nom;
+            Ville = ville;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Ville { get => ville; set => ville = value; }
+
+        public override string? ToString() // meme chose dans equipe puis generer les substitutions
+        {
+            return nom + " de " + ville;
+        }
     }
 }
